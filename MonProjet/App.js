d08e4db563a7a030e39f6dec99cf9e3a833c202c
MaudelineMarlier <49815873+MaudelineMarlier@users.noreply.github.com>
2020-03-03 16:51:14 +0100

    Mise en page - page d'accueil

--- MonProjet/App.js
+++ MonProjet/App.js
@@ -20,30 +20,84 @@ import { TextInput } from "react-native-gesture-handler";
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => navigation.navigate("Jupiter")}>
-        <ImageBackground
-          source={require("./jupiter1.jpg")}
-          style={{ width: 110, height: 110, marginBottom: 20 }}
-        >
-          <Text style={{ color: "white", textAlign: "center" }}>{"\n"}</Text>
-        </ImageBackground>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("Saturne")}>
-        <ImageBackground
-          source={require("./saturne1.jpg")}
-          style={{ marginTop: 5, width: 120, height: 100, marginBottom: 20 }}
-        >
-          <Text style={{ color: "white", textAlign: "center" }}>{"\n"}</Text>
-        </ImageBackground>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("Terre")}>
-        <ImageBackground
-          source={require("./terre1.jpg")}
-          style={{ width: 110, height: 110, marginBottom: 20 }}
-        >
-          <Text style={{ color: "white", textAlign: "center" }}>{"\n"}</Text>
-        </ImageBackground>
-      </TouchableOpacity>
+      <View style={styles.lignePlanete}>
+        <TouchableOpacity onPress={() => navigation.navigate("Jupiter")}>
+          <ImageBackground
+            source={require("./jupiter1.jpg")}
+            style={{ width: 110, height: 110, marginTop: 60 }}
+          >
+            <Text style={{ color: "white", textAlign: "center" }}>{"\n"}</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Saturne")}>
+          <ImageBackground
+            source={require("./saturne1.jpg")}
+            style={{ width: 120, height: 100, marginBottom: 30 }}
+          >
+            <Text style={{ color: "white", textAlign: "center" }}>{"\n"}</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Terre")}>
+          <ImageBackground
+            source={require("./terre1.jpg")}
+            style={{ width: 110, height: 110, marginTop: 60 }}
+          >
+            <Text style={{ color: "white", textAlign: "center" }}>{"\n"}</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.lignePlanete}>
+        <TouchableOpacity onPress={() => navigation.navigate("Mars")}>
+          <ImageBackground
+            source={require("./mars1.jpg")}
+            style={{ width: 110, height: 110 }}
+          >
+            <Text style={{ color: "white", textAlign: "center" }}>{"\n"}</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Soleil")}>
+          <ImageBackground
+            source={require("./soleil1.jpg")}
+            style={{ width: 140, height: 140, marginLeft: 10 }}
+          >
+            <Text style={{ color: "white", textAlign: "center" }}>{"\n"}</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Mercure")}>
+          <ImageBackground
+            source={require("./mercure1.jpg")}
+            style={{ width: 110, height: 110 }}
+          >
+            <Text style={{ color: "white", textAlign: "center" }}>{"\n"}</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.lignePlanete}>
+        <TouchableOpacity onPress={() => navigation.navigate("Neptune")}>
+          <ImageBackground
+            source={require("./neptune1.jpg")}
+            style={{ width: 110, height: 110, marginBottom: 30 }}
+          >
+            <Text style={{ color: "white", textAlign: "center" }}>{"\n"}</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Uranus")}>
+          <ImageBackground
+            source={require("./uranus1.jpg")}
+            style={{ width: 100, height: 113, marginTop: 40 }}
+          >
+            <Text style={{ color: "white", textAlign: "center" }}>{"\n"}</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate("Venus")}>
+          <ImageBackground
+            source={require("./venus1.jpg")}
+            style={{ width: 110, height: 110, marginBottom: 30 }}
+          >
+            <Text style={{ color: "white", textAlign: "center" }}>{"\n"}</Text>
+          </ImageBackground>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -81,11 +135,10 @@ function JouerJupiterScreen({ navigation }) {
             borderColor: "black",
             backgroundColor: "lightgrey"
           }}
+          onSubmitEditing={text => onChangeText(text)}
         ></TextInput>
         <Text style={{}}> dans le noir </Text>
       </View>
-
-      <Button title="Jouer !" onPress={() => navigation.navigate("Jouer")} />
     </View>
   );
 }
@@ -104,6 +157,48 @@ function TerreScreen({ navigation }) {
     </View>
   );
 }
+function MarsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="On ne peut pas encore jouer" />
+    </View>
+  );
+}
+function SoleilScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="On ne peut pas encore jouer" />
+    </View>
+  );
+}
+function MercureScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="On ne peut pas encore jouer" />
+    </View>
+  );
+}
+function NeptuneScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="On ne peut pas encore jouer" />
+    </View>
+  );
+}
+function UranusScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="On ne peut pas encore jouer" />
+    </View>
+  );
+}
+function VenusScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="On ne peut pas encore jouer" />
+    </View>
+  );
+}
 
 const Stack = createStackNavigator();
 
@@ -114,7 +209,7 @@ function MyStack() {
         name="Accueil"
         component={HomeScreen}
         options={{
-          title: "Jupiter",
+          title: "Bienvue dans notre système solaire !",
           headerStyle: {
             backgroundColor: "#f4511e"
           },
@@ -162,7 +257,79 @@ function MyStack() {
         name="Terre"
         component={TerreScreen}
         options={{
-          title: "Le Terre vous accueille",
+          title: "La Terre vous accueille",
+          headerStyle: {
+            backgroundColor: "#f4511e"
+          },
+          headerTintColor: "#fff",
+          headerTitleAlign: "center"
+        }}
+      />
+      <Stack.Screen
+        name="Mars"
+        component={MarsScreen}
+        options={{
+          title: "Mars vous accueille",
+          headerStyle: {
+            backgroundColor: "#f4511e"
+          },
+          headerTintColor: "#fff",
+          headerTitleAlign: "center"
+        }}
+      />
+      <Stack.Screen
+        name="Soleil"
+        component={SoleilScreen}
+        options={{
+          title: "Le Soleil vous accueille",
+          headerStyle: {
+            backgroundColor: "#f4511e"
+          },
+          headerTintColor: "#fff",
+          headerTitleAlign: "center"
+        }}
+      />
+      <Stack.Screen
+        name="Mercure"
+        component={MercureScreen}
+        options={{
+          title: "Mercure vous accueille",
+          headerStyle: {
+            backgroundColor: "#f4511e"
+          },
+          headerTintColor: "#fff",
+          headerTitleAlign: "center"
+        }}
+      />
+      <Stack.Screen
+        name="Neptune"
+        component={NeptuneScreen}
+        options={{
+          title: "Neptune vous accueille",
+          headerStyle: {
+            backgroundColor: "#f4511e"
+          },
+          headerTintColor: "#fff",
+          headerTitleAlign: "center"
+        }}
+      />
+      <Stack.Screen
+        name="Uranus"
+        component={UranusScreen}
+        options={{
+          title: "Uranus vous accueille",
+          headerStyle: {
+            backgroundColor: "#f4511e"
+          },
+          headerTintColor: "#fff",
+          headerTitleAlign: "center"
+        }}
+      />
+      <Stack.Screen
+        name="Venus"
+        component={VenusScreen}
+        options={{
+          title: "Venus vous accueille",
           headerStyle: {
             backgroundColor: "#f4511e"
           },
@@ -185,12 +352,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
-    flexDirection: "row"
+    backgroundColor: "blue",
+    alignItems: "center"
   },
-  soleil: {
+  lignePlanete: {
     alignItems: "center",
-    top: 10,
-    left: 10
+    flexDirection: "row"
   }
 });
